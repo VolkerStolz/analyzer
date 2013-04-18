ffdb3cac76247eca43269590f750f0e4b6437a56
Volker Stolz <stolz@ifi.uio.no>
2013-04-18 16:04:05 +0200

    remove cruft that's already in MayLocks

--- src/analyses/slps.ml
+++ src/analyses/slps.ml
@@ -12,41 +12,9 @@ struct
   module Dom  = LockDomain.SLPset
   module Glob = Glob.Make (Lattice.Unit)
   
-  (* BEGIN directly hoisted from MayLocks *)
-   
-  (* Helper function to convert query-offsets to valuedomain-offsets *)
-  let rec conv_offset x =
-    match x with
-      | `NoOffset    -> `NoOffset
-      | `Index (Const (CInt64 (i,_,_)),o) -> `Index (ValueDomain.IndexDomain.of_int i, conv_offset o)
-      | `Index (_,o) -> `Index (ValueDomain.IndexDomain.top (), conv_offset o)
-      | `Field (f,o) -> `Field (f, conv_offset o)
-  
-  (* Query the value (of the locking argument) to a list of locks. *)
-  let eval_exp_addr a exp =
-    let gather_addr (v,o) b = ValueDomain.Addr.from_var_offset (v,conv_offset o) :: b in
-    match a (Queries.MayPointTo exp) with
-      | `LvalSet a when not (Queries.LS.is_top a) ->
-          Queries.LS.fold gather_addr (Queries.LS.remove (dummyFunDec.svar, `NoOffset) a) []
-      | b -> Messages.warn ("Could not evaluate '"^sprint 30 (d_exp () exp)^"' to an points-to set, instead got '"^Queries.Result.short 60 b^"'."); []
- 
-  let lock rw may_fail a lv arglist ls : (Dom.ReverseAddrSet.t * Cil.exp * bool) list =
-    let set_ret tv sts = 
-      match lv with 
-        | None -> (sts,Cil.integer 1,true)
-        | Some lv -> (sts,Lval lv,tv)
-    in 
-    let lock_one xs (e:LockDomain.Addr.t) =
-        (set_ret false  (Dom.add (e,rw) ls)) ::
-        if may_fail then set_ret true ls :: xs else xs
-    in
-    List.fold_left lock_one [] (List.concat (List.map (eval_exp_addr a) arglist)) 
-  
   let startstate () = Dom.empty ()
   let otherstate () = Dom.empty ()
   let exitstate  () = Dom.top ()
-
-  (* END *)
 
   (* transfer functions : Don't propagate anything *)
   let assign ctx (lval:lval) (rval:exp) : Dom.t = Dom.empty ()
@@ -64,10 +32,10 @@ struct
         | [`MayLocks l] -> l
         | _ -> failwith "SLP unavailable --- dependencies missing!"
     in
-    (* locking logic for SLPs. *)
+    (* locking logic for SLPs. Refer to MayLocks for resolving aliases. *)
     match (LibraryFunctions.classify f.vname arglist, f.vname) with
       | `Lock (failing, rw), _
-          when not (Dom.is_empty maylocks) -> lock rw failing ctx.ask lval arglist ctx.local
+          when not (Dom.is_empty maylocks) -> MayLocks.Spec.lock rw failing ctx.ask lval arglist ctx.local
       | _ -> [Dom.empty (), Cil.integer 1, true]
 
 end
